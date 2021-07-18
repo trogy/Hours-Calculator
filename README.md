7d64ddaa05deb54c3c5ed27b30f2894d030861d4
trogy <33707105+trogy@users.noreply.github.com>
2021-07-18 15:24:42 +1200

    Add everything so far

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Hours Calculator
  Calculate weekly hours
+
+ ##Installation
+ 
+ ###### Desktop
+ Download the installer or zip file this is bundled with PHP Desktop and install or open the executable.
+ ###### Web Server [More Advanced]
+ Install XAMPP or a LAMP stack [Your Choice] drag the contents of www to www for LAMP or htdocs for XAMPP
---
+++ www/Index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Hours Calculator</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<style>
+:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: .75rem;
+}
+
+body {
+  background: #007bff;
+  background: linear-gradient(to right, #0062E6, #33AEFF);
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 2rem;
+}
+
+.form-label-group>input,
+.form-label-group>label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group>label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown)~label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+
+.btn-google {
+  color: white;
+  background-color: #ea4335;
+}
+
+.btn-facebook {
+  color: white;
+  background-color: #3b5998;
+}
+
+/* Fallback for Edge
+-------------------------------------------------- */
+
+@supports (-ms-ime-align: auto) {
+  .form-label-group>label {
+    display: none;
+  }
+  .form-label-group input::-ms-input-placeholder {
+    color: #777;
+  }
+}
+
+/* Fallback for IE
+-------------------------------------------------- */
+
+@media all and (-ms-high-contrast: none),
+(-ms-high-contrast: active) {
+  .form-label-group>label {
+    display: none;
+  }
+  .form-label-group input:-ms-input-placeholder {
+    color: #777;
+  }
+}
+</style>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+           <h5 class="card-title text-center">Hours Calculator</h5>
+            <hr class="my-4">
+            <form class="form-signin" action="Calculator.php" method="POST">
+              <p> Monday </p>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D1_Start" id="D1_Start" class="form-control" required>
+              </div>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D1_End" id="D1_End" class="form-control" required>
+              </div>
+              <hr>
+              <p> Tuesday </p>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D2_Start" id="D2_Start" class="form-control" required>
+              </div>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D2_End" id="D2_End" class="form-control" required>
+              </div>
+              <hr>
+              <p> Wednesday </p>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D3_Start" id="D3_Start" class="form-control" required>
+              </div>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D3_End" id="D3_End" class="form-control" required>
+              </div>
+              <hr>
+              <p> Thursday </p>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D4_Start" id="D4_Start" class="form-control" required>
+              </div>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D4_End" id="D4_End" class="form-control" required>
+              </div>
+              <hr>
+              <p> Friday </p>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D5_Start" id="D5_Start" class="form-control" required>
+              </div>
+              <div class="form-label-group">
+                <input type="datetime-local" name="D5_End" id="D5_End" class="form-control" required>
+              </div>
+              <hr>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
